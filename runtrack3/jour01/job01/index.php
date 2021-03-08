16a1ba450af63a369701dca077865fb882f6a2f4
dorian.di-russo <71875004+dorian-di-russo@users.noreply.github.com>
2021-03-08 15:56:02 +0100

    Add files via upload

---
+++ runtrack3/jour01/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+<html>
+  <head>
+    <title>Ma page Web</title>
+    <script type="text/javascript">
+      alert('hello Javascript!');
+    </script>
+  </head>
+
+<body>
+    
+</body>
+</html>
---
+++ runtrack3/jour01/job05/script.js
@@ -0,0 +1,33 @@
+//const array1 = ['lundi', 'mardi', 'mercredi','jeudi','vendredi','samedi','dimanche'];
+
+
+
+//array1.forEach(element => console.log(element));
+
+/*var joursdelasemaine = ['lundi', 'mardi', 'mercredi','jeudi','vendredi','samedi','dimanche']
+function joursdelasemaine (){
+   
+    for (var i = 0; i < joursdelasemaine.length; i++) {
+
+       
+
+        array1.joursdelasemaine(element => console.log(element));
+     
+                    
+     
+     }
+}
+*/
+
+
+var jours = ['lundi', 'mardi', 'mercredi','jeudi','vendredi','samedi','dimanche']
+function joursdelasemaine(){
+for (var i=0; i< jours.length; i++)
+
+{
+    console.log(jours[i]);
+}
+
+}
+
+console.log(joursdelasemaine());
